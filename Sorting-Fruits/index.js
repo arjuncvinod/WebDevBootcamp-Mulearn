b86483a0a4d05b1cef2cd9a15496b5cd7293305b
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-13 22:10:04 +0530

    Step 11

---
+++ Sorting-Fruits/index.js
@@ -0,0 +1,16 @@
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊", "🍊", "🍊","🍊"];
+let appleShelf = document.getElementById("apple-shelf");
+let orangeShelf = document.getElementById("orange-shelf");
+
+
+function sortFruit() {
+  for (let i = 0; i < fruit.length; i++) {
+    if (fruit[i] === "🍎") {
+      appleShelf.textContent += "🍎";
+    } else if (fruit[i] === "🍊") {
+      orangeShelf.textContent += "🍊";
+    }
+  }
+}
+
+sortFruit();
