659543861dc8f5e3a9dfb2677cabb7bc89916f0f
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-24 23:01:40 +0530

    upadated

--- react-info/src/components/NavBar.jsx
+++ react-info/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav>
-      <img src="../images/logo512.png" className="nav--icon" alt="" />
+      <img src="react-icon-small.png" className="nav--icon" alt="" />
       <h3 className="nav--logo_text">ReactFacts</h3>
       <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
