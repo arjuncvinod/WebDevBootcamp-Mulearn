bcc9eaeec8ba125ccf11adfffc3459f70ff5cc89
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-29 01:01:39 +0530

    step 16

--- airbnb/src/App.jsx
+++ airbnb/src/App.jsx
@@ -3,26 +3,17 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
 import data from "./data";
-import "./styles.css"
+
 export default function App() {
-  // <Hero />
   const cards = data.map((item) => {
-    return (
-      <Card
-        img={item.coverImg}
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        location={item.location}
-        title={item.title}
-        price={item.price}
-      />
-    );
+    return <Card key={item.id} {...item} />;
   });
 
   return (
     <div>
       <Navbar />
-      {cards}
+      <Hero />
+      <section className="cards-list">{cards}</section>
     </div>
   );
 }
--- airbnb/src/components/Card.jsx
+++ airbnb/src/components/Card.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 
 export default function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
   return (
     <div className="card">
-      <img src={`../images/${props.img}`} className="card--image" />
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img src={`../images/${props.coverImg}`} className="card--image" alt=""/>
       <div className="card--stats">
-        <img src="../images/star.png" className="card--star" />
-        <span>{props.rating}</span>
-        <span className="gray">({props.reviewCount}) • </span>
+        <img src="../images/star.png" className="card--star" alt="" />
+        <span>{props.stats.rating}</span>
+        <span className="gray">({props.stats.reviewCount}) • </span>
         <span className="gray">{props.location}</span>
       </div>
-      <p>{props.title}</p>
-      <p>
+      <p className="card--title">{props.title}</p>
+      <p className="card--price">
         <span className="bold">From ${props.price}</span> / person
       </p>
     </div>
