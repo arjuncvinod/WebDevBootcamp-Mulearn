362a65260d31c86247b42903160d475fd71d3a3e
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-28 23:54:58 +0530

    updated

---
+++ airbnb/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="../images/airbnb-logo.png" className="nav--logo" alt="" />
+    </nav>
+  );
+}
