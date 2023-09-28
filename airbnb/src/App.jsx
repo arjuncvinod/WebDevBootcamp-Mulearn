2bffc811c766926cc0ed5fa0768ddac1ee2826bc
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-28 23:54:12 +0530

    updated

--- airbnb/src/App.jsx
+++ airbnb/src/App.jsx
@@ -1,7 +1,40 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import data from "./data";
 import "./styles.css"
-function App() {
+/*
+Challenge:
+
+- import the array of data from data.js
+- map over the array to create <Card /> components
+- display the array of card components under the navbar
+  (in place of the current <Card /> component)
+
+Note: We haven't styled the group of components yet, so they'll
+still be block elements, stacked vertically. We'll add styling later.
+*/
+
+export default function App() {
+  // <Hero />
+  const cards = data.map((item) => {
+    return (
+      <Card
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    );
+  });
+
   return (
-   <h1>hello</h1>
+    <div>
+      <Navbar />
+      {cards}
+    </div>
   );
 }
-export default App;
