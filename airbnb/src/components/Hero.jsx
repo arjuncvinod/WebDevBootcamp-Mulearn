d08a3ff1ddb8568eb42217f2ec7931ba748f7eaf
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-29 01:07:10 +0530

    step 16

--- airbnb/src/components/Hero.jsx
+++ airbnb/src/components/Hero.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function Hero() {
   return (
     <section className="hero">
-      <img src="../images/photo-grid.png" className="hero--photo" />
+      <img src="../images/photo-grid.png" className="hero--photo" alt="" />
       <h1 className="hero--header">Online Experiences</h1>
       <p className="hero--text">
         Join unique interactive activities led by one-of-a-kind hosts—all
-        without leaving home.
+        without leaving Home.
       </p>
     </section>
   );
