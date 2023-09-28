8a471e695b30a13a9694ea01c1b4d26d8b5add91
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-28 23:56:43 +0530

    updated

--- airbnb/src/App.jsx
+++ airbnb/src/App.jsx
@@ -4,18 +4,6 @@ import Hero from "./components/Hero";
 import Card from "./components/Card";
 import data from "./data";
 import "./styles.css"
-/*
-Challenge:
-
-- import the array of data from data.js
-- map over the array to create <Card /> components
-- display the array of card components under the navbar
-  (in place of the current <Card /> component)
-
-Note: We haven't styled the group of components yet, so they'll
-still be block elements, stacked vertically. We'll add styling later.
-*/
-
 export default function App() {
   // <Hero />
   const cards = data.map((item) => {
