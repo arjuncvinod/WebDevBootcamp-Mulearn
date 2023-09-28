9a217674c030eefba3fec366576cde9490f67dca
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-28 23:54:53 +0530

    updated

---
+++ airbnb/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <img src="../images/photo-grid.png" className="hero--photo" />
+      <h1 className="hero--header">Online Experiences</h1>
+      <p className="hero--text">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </section>
+  );
+}
