1477fd0c27efe74ff1d930be78968e4c8bcd292d
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-02-16 23:09:05 +0530

    index,js craeted

---
+++ Youtube-Clone/src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Navbar} from './Navbar'
+export {default as Feed} from './Feed'
+export {default as VideoDetails} from './VideoDetails'
+export {default as ChannelDetails} from './ChannelDetails'
+export {default as SearchFeed} from './SearchFeed'
