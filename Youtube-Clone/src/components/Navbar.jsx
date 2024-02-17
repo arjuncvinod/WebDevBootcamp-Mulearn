2ba11d1aed93bc1cace458624ad2a81aef663b80
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-02-17 22:18:30 +0530

    yt-clone updated

--- Youtube-Clone/src/components/Navbar.jsx
+++ Youtube-Clone/src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-function Navbar() {
-  return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 
-export default Navbar
+import { logo } from "../utils/constants";
+import { SearchBar } from "./";
+
+const Navbar = () => (
+  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+    <SearchBar />
+  </Stack>
+);
+
+export default Navbar;
