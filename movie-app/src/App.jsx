d39ec34a27afc67a453d3dac50870ddc991a6cfb
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-02-22 17:19:20 +0530

    lvl 5 task 8 updated

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+const API_URL = "https://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
